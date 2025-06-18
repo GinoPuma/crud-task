dfb3662ab8ff3d234806ba86f602d71cf86a3b8a
GinoPuma <ginopuma8@gmail.com>
2025-06-18 18:29:21 -0500

    a

--- frontend/src/components/ClienteTable.jsx
+++ frontend/src/components/ClienteTable.jsx
@@ -3,38 +3,29 @@ export default function ClienteTable({ clientes, onDelete, onEdit }) {
     <table border="1" cellPadding="5">
       <thead>
         <tr>
-          <th>Nro. de Orden</th> 
-          <th>Nombre</th>
+          <th>NRO. ORDEN</th>
+          <th>NOMBRE</th>
           <th>DNI</th>
-          <th>Empresa</th>
-          <th>Celular</th>
-          <th>Acciones</th>
+          <th>EMPRESA</th>
+          <th>CELULAR</th>
+          <th>ACCIONES</th>
         </tr>
       </thead>
       <tbody>
-        {clientes.map(
-          (
-            cli,
-            index 
-          ) => (
-            <tr key={cli.id}>
-              <td>{index + 1}</td>{" "}
-              <td>{cli.nombre}</td>
-              <td>{cli.dni}</td>
-              <td>{cli.empresa}</td>
-              <td>{cli.celular}</td>
-              <td>
-                <button onClick={() => onDelete(cli.id)}>Eliminar</button>
-                <button
-                  onClick={() => onEdit(cli)}
-                  style={{ marginLeft: "5px" }}
-                >
-                  Editar
-                </button>
-              </td>
-            </tr>
-          )
-        )}
+        {clientes.map((cli, index) => (
+          <tr key={cli.id}>
+            <td>{index + 1}</td> <td>{cli.nombre}</td>
+            <td>{cli.dni}</td>
+            <td>{cli.empresa}</td>
+            <td>{cli.celular}</td>
+            <td>
+              <button onClick={() => onDelete(cli.id)}>Eliminar</button>
+              <button onClick={() => onEdit(cli)} style={{ marginLeft: "5px" }}>
+                Editar
+              </button>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
